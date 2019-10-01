48b7a687e3bee68ce41c8db7d57c1e2e96d5b50c
Vitaly Salmin <vitaliy.salmin.95@mail.ru>
2019-10-01 22:53:12 +0300

    finished the tasks

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,19 +1,41 @@
+// TASK 1
+"use strict";
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
     let date = window.date.value;
-
     let result = calculateTotalMortgage(percent, contribution, amount, date);
     let span = window.mortageResult;
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+  if (typeof amount === 'string') {
+    amount = parseInt(amount);
+  }
+  if (typeof contribution === 'string') {
+    contribution = parseInt(contribution);
+  }
+  
+function calculateTotalMortgage(percentage, firstPayment, sum, endDate) {
+  let date1 = new Date();
+  let date2 = new Date(endDate);
+  let diffTime = Math.abs(date2 - date1);
+  let months = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 30.4)); 
+  if (typeof months === 'string') {
+    months = parseInt(months);
+  }
+  percentage = percentage/1200;
+  let monthlyPayment = sum*((percentage + percentage/((Math.pow((1+percentage), months)-1))));
+  let totalPayment = monthlyPayment * months + contribution;
+  if (typeof totalPayment === 'string') {
+  	totalPayment = parseFloat(totalPayment);
+  }
+  return totalPayment.toFixed(2);
 }
+
+
+// TASK 2
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +45,11 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if ((typeof name !== 'string' && typeof name !== 'number') || !name.replace(/\s/g, '').length) {
+    	name = "Аноним";
+    }
+    console.log(name.length);
+    console.log(typeof name);
+    let greeting = `Привет мир! Меня зовут ${name}`;
+    return greeting;
 }
