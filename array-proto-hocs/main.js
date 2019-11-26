f2c71f319637347cc87ebb7631f1c5594f43780c
Vitaly Salmin <vitalysalmin@yandex.ru>
2019-11-26 21:43:24 +0300

    corrected sum function and what the main function returns

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -8,16 +8,23 @@ let compareArrays = (aOne, aTwo) => {
   return aOne.length === aTwo.length && aOne.every((value, index) => value === aTwo[index])
 }
 
-let sum = (ar) => {return ar.reduce((a, b) => a + b, 0)}
+let findResult = (arrayToSearch, arrayToCompareWith) => {
+  return arrayToSearch.find(result => compareArrays(result.args, arrayToCompareWith));
+}
+
+/* я сделал как вы сказали(добавил rest к аргументу sum и добавил spread при вызове sum как колбэк функции - к аргументу fn) и вы, разумеется, наверное, правы, просто мне казалось, когда я про них читал, что rest оператор позволяет потенциальные аргументы сделать как бы массивом, а spread наоборот -переданный массив-аргумент разделить, т.е. учитывая, что возвращаемая функция принимает произвольное кол-во аргументов(через  rest опять же) и мы массив передаем этот в колбэк функцию, добавляя rest к  sum мы вынуждены вызвать ее как колбэк функцию со spread -    т.е. по сути по ходу вызова функций сделать конвертацию аргумента возвр. функции туда обратно, чтобы прийти к тому же результату. Извините, что длинно так) Возможно, что я неправильно что-то понял. Я понимаю, что чтобы не запутаться так дальше будет проще работать с функциями, наверное + требование, что именно сам sum должен принимать произвольное кол-во аргументов и не требовать оптимизации */
+
+let sum = (...ar) => {return ar.reduce((a, b) => a + b, 0)}
 
 function memorize(fn, limit) {
   let results = [];
   
   return function(...arr) {
-    try {
-      console.log(`the result is found in memory, sum - ${(results.find(result => compareArrays(result.args, arr))).result}`);
-    } catch {
-        let newResult = fn(arr);
+    if (findResult(results, arr) !== undefined) {
+      console.log(`the result is found in memory, sum - ${findResult(results, arr).result} \n`);
+      return findResult(results, arr).result;
+    } else {
+        let newResult = fn(...arr);
         if(results.length >= limit) {
           console.log("memory limit exceeded, removing the   oldest entry to add a new one");
           console.log("calculating sum");
@@ -27,7 +34,8 @@ function memorize(fn, limit) {
         } else { 
           console.log("calculating and adding a new entry to the memory");
           console.log(`sum - ${newResult} \n`);
-          return results.push({ args: arr, result: newResult});
+          results.push({ args: arr, result: newResult});
+          return newResult;
       }
     }
   }
@@ -36,7 +44,7 @@ function memorize(fn, limit) {
 let mSum = memorize(sum, 2);
 mSum(1, 3);
 mSum(1, 3);
-smSum2, 5);
+mSum(2, 5);
 mSum(3, 7);
 mSum(2, 5);
 mSum(4, 6);
